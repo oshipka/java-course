1a48e8b86126dc3f655d50eea4ce55e80bfb0ec2
oshipka <olenkashipka@gmail.com>
2019-03-03 23:00:20 +0200

    completed part 1 of task

---
+++ src/LettersTask.java
@@ -0,0 +1,2 @@
+public class LettersTask {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
-public class Main {
 
-    public static void main(String[] args) {
+public class Main {
+    static MatrixTask pt1 = new MatrixTask();
+
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
+        pt1.CompleteTask();
     }
+
+
+
 }
