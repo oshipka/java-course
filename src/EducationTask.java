775eb600322218e874ba3d18a976e6a4e07304d6
oshipka <olenkashipka@gmail.com>
2019-03-04 17:23:40 +0200

    completed part 3 of task

--- src/EducationTask.java
+++ src/EducationTask.java
@@ -1,2 +1,180 @@
-public class EducationTask {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class EducationTask
+{
+	
+	
+	static class Institution
+	{
+		String name;
+		String address;
+		Calendar foundationDate = Calendar.getInstance();
+		
+		Institution() throws ParseException
+		{
+			name = "";
+			address = "";
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+			foundationDate.setTime(sdf.parse("1900"));
+		}
+		
+		Institution(String str_name, String str_addr, String fndD) throws ParseException
+		{
+			name = str_name;
+			address = str_addr;
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+			foundationDate.setTime(sdf.parse(fndD));
+		}
+		
+		public static void PrintInst(Institution[] institutions)
+		{
+			for (Institution institution: institutions)
+			{
+				System.out.println(institution.ToString());
+			}
+		}
+		
+		public static void OrderByYear(Institution[] institutions)
+		{
+			int len = institutions.length;
+			for (int i = 0; i<len; i++)
+			{
+				for(int j = 0; j<len-1; j++)
+				{
+					if(institutions[j].foundationDate.get(Calendar.YEAR)>institutions[j+1].foundationDate.get(Calendar.YEAR))
+					{
+						Institution temp = institutions[j];
+						institutions[j] = institutions[j+1];
+						institutions[j+1] = temp;
+					}
+				}
+				PrintInst(institutions);
+			}
+		}
+		
+		public String ToString()
+		{
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+			
+			String result = "";
+			result += this.name + " "
+					+ this.address + " "
+					+ sdf.format(this.foundationDate.getTime()) + " ";
+			return result;
+		}
+	}
+	
+	static class School extends Institution
+	{
+		Integer studentsAmount;
+		Integer number;
+		
+		
+		School() throws ParseException
+		{
+			super();
+			studentsAmount = 0;
+			number = 0;
+		}
+		
+		School(String str_name, String str_addr, String fndD, Integer stdAm, Integer num) throws ParseException
+		{
+			super(str_name, str_addr, fndD);
+			studentsAmount = stdAm;
+			number = num;
+		}
+		
+		public int GetStudentsAmount()
+		{
+			return studentsAmount;
+		}
+		
+		public static void LeastStudents(Institution[] institutions) throws ParseException
+		{
+			School result = new School();
+			
+			for (Institution institution : institutions)
+			{
+				if (institution instanceof School)
+				{
+					School curr = (School) institution;
+					if (result.studentsAmount > curr.studentsAmount)
+					{
+						result = curr;
+					}
+				}
+			}
+			System.out.println(result.ToString());
+		}
+	
+		public String ToString()
+		{
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+			
+			String result = "";
+			result += this.name + " "
+					+ this.address + " "
+					+ sdf.format(this.foundationDate.getTime()) + " "
+					+ studentsAmount.toString() + " "
+					+ number.toString() + " ";
+			return result;
+		}
+	}
+	
+	static class University extends Institution
+	{
+		Integer accreditation;
+		Integer facultiesAmount;
+		
+		public University() throws ParseException
+		{
+			super();
+			accreditation = 0;
+			facultiesAmount = 0;
+		}
+		
+		University(String str_name, String str_addr, String fndD, Integer accr, Integer facAm) throws ParseException
+		{
+			super(str_name, str_addr, fndD);
+			accreditation = accr;
+			facultiesAmount = facAm;
+		}
+		
+		public int GetAccLevel()
+		{
+			return accreditation;
+		}
+		
+		public static void GetAllOfAccLevel(Institution[] institutions, int accLevel)
+		{
+			for (Institution institution : institutions)
+			{
+				if (institution instanceof University)
+				{
+					University curr = (University) institution;
+					if (curr.accreditation == accLevel)
+					{
+						System.out.print(curr.ToString() + ", ");
+					}
+				}
+			}
+		}
+		
+		public String ToString()
+		{
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy", Locale.ENGLISH);
+			
+			String result = "";
+			result += this.name + " "
+					+ this.address + " "
+					+ sdf.format(this.foundationDate.getTime()) + " "
+					+ accreditation.toString() + " "
+					+ facultiesAmount.toString() + " ";
+			return result;
+		}
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,47 @@
+import java.text.ParseException;
 
-public class Main {
-    private static MatrixTask pt1 = new MatrixTask();
-    private static LettersTask pt2 = new LettersTask();
-    private static EducationTask pt3 = new EducationTask();
-
-    public static void main(String[] args)
-    {
-        System.out.println("1. Max sums in line:");
-        pt1.CompleteTask();
-        System.out.println("2. T in words replaced with th:");
-        pt2.CompleteTask();
-        //pt3.CompleteTask();
-    }
-
-
-
+public class Main
+{
+	private static MatrixTask pt1 = new MatrixTask();
+	private static LettersTask pt2 = new LettersTask();
+	
+	public static void main(String[] args)
+	{
+		System.out.println("1. Max sums in line:");
+		pt1.CompleteTask();
+		System.out.println("2. T in words replaced with th:");
+		pt2.CompleteTask();
+		
+		try
+		{
+			EducationTask.Institution[] institutions = new EducationTask.Institution[]
+				{
+					new EducationTask.School("Gymnasium Prestige", "Lviv", "1987", 517, 53),
+					new EducationTask.School("Solonka School", "Solonka", "1934", 326, 32),
+					new EducationTask.University("Ivan Franko national university of Lviv", "Universytetska Street 1 Lviv (population range: 500,000-1,000,000) 79000 Lviv Oblast Ukraine", "1661", 4, 12),
+					new EducationTask.University("Taras Shevchenko national university of Kyiiv", "Volodymyrska Street, 64/13 Kiev (population range: 1,000,000-5,000,000) 01601 Kiev Oblast Ukraine", "1834", 4, 13),
+					new EducationTask.School("School 1", "Lviv", "1897", 412, 13),
+					new EducationTask.School("School 2", "Kyiv", "1998", 201, 16),
+					new EducationTask.University("National Technical University of Ukraine Kyiv Polytechnic Institute", "Peremohy Street 37 Kiev (population range: 1,000,000-5,000,000) 03056 Kiev Oblast Ukraine", "1898", 1, 5),
+					new EducationTask.School("School 3", "Sumy", "1975", 307, 4),
+					new EducationTask.University("Sumy State University", "Rymskiy Korsakov Street, 2 Sumy (population range: 250,000-499,999) 40007 Sumy Oblast Ukraine", "1993", 2, 10),
+					new EducationTask.University("Borys Grinchenko Kyiv University", "Vorovskogo Street, 18/2 Kiev (population range: 1,000,000-5,000,000) 04053 Kiev Oblast Ukraine", "1919", 3, 7)
+				};
+			
+			
+			System.out.println("3. List of all institutions:");
+			EducationTask.Institution.PrintInst(institutions);
+			System.out.println("3.1 Institutions ordered in order of  their foundation time:");
+			EducationTask.Institution.OrderByYear(institutions);
+			System.out.println("3.2 School with least students:");
+			EducationTask.School.LeastStudents(institutions);
+			System.out.println("3.3 Universities with given level of accreditation:");
+			EducationTask.University.GetAllOfAccLevel(institutions, 4);
+			
+		} catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
 }
